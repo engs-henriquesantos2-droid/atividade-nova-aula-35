05b92d27700c46f6e6e0e40be4c17d9515a210c8
henr <engs-henriquesantos2@camporeal.edu.br>
2026-04-30 20:57:17 -0300

    .

---
+++ atividade .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    unsigned long long fatorial = 1;
+
+    cout << "Digite um número inteiro: ";
+    cin >> numero;
+
+    if (numero < 0) {
+        cout << "Número negativo!" << endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= numero; i++) {
+        fatorial *= i;
+    }
+
+    cout << numero << "! = " << fatorial << endl;
+    return 0;
+} 
+e 
